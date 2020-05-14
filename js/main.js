9d42b024bda4594fe29dc7cc8ba7786ce61f1d0e
xiaoke <519871893@qq.com>
2020-05-14 17:17:45 +0800

    点击商品数量添加

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
         }
     })
 
-////点击购物车下拉
+//点击购物车下拉
 //    $(".shoppingDropD").mouseover(function(){
 //        $(this).find(".dropdown-menu").show();
 //    }).mouseout(function(){
@@ -43,190 +43,193 @@ $(document).ready(function() {
 //    }).mouseout(function(){
 //        $(this).hide();
 //    });
-//
-//
-//    //计算总价与小计
-//    function prodC() {
-//        var $tr = $(".dropMenu-content").find("li");
-//        var summer = 0;
-//        $tr.each(function(i, dom) {
-//            //console.log(dom);
-//            var num = $(dom).find(".input-group-add span").text(); //商品数量
-//            //console.log(num);
-//            var jiage= $(dom).find(".priceAndNumber h3").text();
-//            var price = num *jiage; //商品小计
-//            $(dom).find(".cartCount span").html(price); //显示商品小计
-//            summer += price; //总价
-//        });
-//        $(".cartCount span").text(summer);
-//    }
-//    prodC(); //页面加载完成后运行
-//    //商品增加减少 ，flag 为true时增加 flag为false时减少
-//    function changN(dom, flag) {
-//        //value = Number(value)
-//        var $value=$(dom).siblings(".number");
-//        var value = $value.text();
-//        //console.log(value)
-//        if(flag) {
-//            value++;
-//        } else {
-//            value--;
-//            if(value < 0) {
-//                value = 0;
-//                alert("宝贝数量不能为负值")
-//            }
-//        }
-//        $value.html(value);
-//        prodC();
-//    };
-//    //点击增加
-//    $(".input-group-add .add1").click(function() {
-//        changN(this, true);
-//    });
-//    //点击减少
-//    $(".input-group-add .jianshao").click(function() {
-//        changN(this, false);
-//    });
-//
-//
-//
-//
-//    /*详情页产品图*/
-//    var oAdd = $('.input-number');//添加购物车按钮
-//    var oPlus = $('.input-group-btn');//商品加
-//    var oReduce = $('.input-group-addon');//商品减
-//    // + 按钮点击事件
-//    $(oPlus).each(function(index,value){
-//        //JQ循环遍历每一个增加num的按钮
-//        /**
-//         * 当点击增加按钮的时候此按钮的上一级兄弟元素中的值加一
-//         * JQ获取当前节点的上一个兄弟节点 节点.prev()
-//         */
-//        $(this).click(function(){//对每个按钮都进行增加的功能,无上限,当前this指向当前oPlus
-//            var num = parseInt($(this).prev().html());//获取当前商品的num值,JQ获取当前元素的紧邻的前一个兄弟元素prev()
-//            num ++;//每点击一次num + 1;
-//            $(this).prev().html(num);//然后将改变过的num值重新更改到当前元素的前一个兄弟元素中(即num框中)
-//        })
-//    })
-//
-//// - 按钮点击事件
-//    $(oReduce).each(function(index,value){
-//        /**
-//         * 当点击减少的按钮的时候,首先先判断一下他的后一个兄弟元素中的值是否已经为0了,若为0了就直接赋值为0,不能让其继续减少,如若大于0则每点击一次减一
-//         * JQ获取当前元素的后一个兄弟元素 节点.next()
-//         */
-//        $(this).click(function(){
-//            var num = parseInt($(this).next().html());
-//            if(num === 0){
-//                num = 0;
-//                $(this).next().html(num);
-//                alert("宝贝数量不能为空");
-//            }
-//            if(num > 0){
-//                num --;
-//                $(this).next().html(num);
-//            }
-//        })
-//
-//    })
 
 
 
-    var btnCar = document.querySelectorAll('.shoppingBtn');
-    //console.log(btnCar[0]);
-    // 载入所有存储在localStorage的数据
-    loadAll();
-    for (let i = 0; i < btnCar.length; i++) {
-        btnCar[i].onclick = function () {
-            //console.log("ok");
-            var productImg = document.getElementsByClassName('productImg')[i].src;
-            //console.log(productImg);
-            var goods = document.getElementsByClassName('productName')[i].innerHTML;
+
+
+    /*购物车添加减少*/
+    var oPlus = $('.add1');//商品加
+    var oReduce = $('.jianshao');//商品减
+    var closeBtn = $('.closeBtn');//商品减
+    // + 按钮点击事件
+    $(oPlus).each(function(index,value){
+        //JQ循环遍历每一个增加num的按钮
+        /**
+         * 当点击增加按钮的时候此按钮的上一级兄弟元素中的值加一
+         * JQ获取当前节点的上一个兄弟节点 节点.prev()
+         */
+        $(this).click(function(){//对每个按钮都进行增加的功能,无上限,当前this指向当前oPlus
+            var num = parseInt($(this).prev().html());//获取当前商品的num值,JQ获取当前元素的紧邻的前一个兄弟元素prev()
+            num ++;//每点击一次num + 1;
+            $(this).prev().html(num);//然后将改变过的num值重新更改到当前元素的前一个兄弟元素中(即num框中)
+        })
+    })
+
+// - 按钮点击事件
+    $(oReduce).each(function(index,value){
+        /**
+         * 当点击减少的按钮的时候,首先先判断一下他的后一个兄弟元素中的值是否已经为0了,若为0了就直接赋值为0,不能让其继续减少,如若大于0则每点击一次减一
+         * JQ获取当前元素的后一个兄弟元素 节点.next()
+         */
+        $(this).click(function(){
+            var num = parseInt($(this).next().html());
+            if(num === 0){
+                num = 0;
+                $(this).next().html(num);
+                alert("宝贝数量不能为空");
+            }
+            if(num > 0){
+                num --;
+                $(this).next().html(num);
+            }
+        })
+
+    })
+
+    //删除数据
+
+    //$(closeBtn).each(function(index,value){
+    //  var children1=$(this).parent().remove();
+    //    localStorage.removeItem("cart");
+    //    ShowCart();
+    //})
+
+
+
+
+
+
+
+
+
+
+
+//var btnCar = document.querySelectorAll('.shoppingBtn');
+    let btnCar = $('.shoppingBtn');
+    //console.log(btnCar);
+
+    $(btnCar).each(function(index,value){
+        count=0;
+        $(this).click(function() {
+            count++;
+            //console.log(count);
+            let img=$(this).parent().parent().find('.productImg').attr("src");
+            //console.log(img);
+            let goods=$(this).parent().find('.productName').html();
             //console.log(goods);
-            var price = document.getElementsByClassName('productPrice')[i].innerHTML;
-            var pid = $(this).parent().parent().attr("pid");
-            data={
-                "productImg":productImg,
-                "goods":goods,
-                'price':price,
-                'pid':pid
+            let price=$(this).parent().find('.productPrice').html();
+            let dataId = $(this).parent().parent().attr("data-id");
+            let num = $("#number").text();
+            //console.log(sum);
+
+            let obj = {
+                img: img,
+                price: price,
+                goods: goods,
+                dataId:dataId,
+                count:count
+            };
+
+            let cart = window.localStorage.getItem('cart');
+            //console.log('localstorage中的数据',cart);
+
+            if(cart) {//存在数据,即购物车不为空
+                //console.log('购物车不为空,执行判断是否有重复数据操作');
+                //然后将获取到的localStorage数据转回为数组
+                let arr = JSON.parse(cart);
+                //console.log('重新转回为数组的localStorage数据', arr);
+                //然后使用数组some的方法判断是否存在重复数据,返回值为布尔值
+                let condition = arr.some(function (item, index, array) {//回调函数有三个参数:1.数组中的元素,2.下标,3.数组本身
+                    return item.dataId === obj.dataId && item.price === obj.price && item.goods === obj.goods;//返回一个经过条件判断之后的一个结果,布尔值
+                });
+                //console.log('判断后的返回值', condition);
+
+                if(condition){
+                    //console.log('判断结果为true执行原数据新数据合并操作');
+                    $(arr).each(function(index,value){//循环遍历这个数组寻找与新数据匹配的元素
+                        //console.log(value);
+                        if(obj.dataId === value.dataId && obj.price === value.price && obj.price === value.price){
+                            //将原新数据的num值合并
+                            //obj.num = parseInt(obj.num)+parseInt(value.num);
+                            //console.log(obj.num);
+                            //删除原数据
+                            arr.splice(index, 1);//删除原数据
+                            arr.push(obj);//新数据增填到数组中
+                            window.localStorage.setItem('cart',JSON.stringify(arr));
+                        }
+                    })
+                }else{
+                    //console.log('判断结果为false直接执行将新数据上传到localStorage中的操作');
+                    arr.push(obj);
+                    window.localStorage.setItem('cart',JSON.stringify(arr));
+                }
+            }else{
+                //console.log('购物车为空执行直接加入往localStorage中添加数据功能');
+                let arr = [obj];
+                window.localStorage.setItem('cart', JSON.stringify(arr))//使用localStrong的setItem来往localStrong中存放数据(两个参数:name,value)
             }
+            ShowCart();//每点击一次调用一次显示购物车功能函数
 
-            //console.log(price);
-            localStorage.setItem('img',productImg);
-            localStorage.setItem('price',price);
-            localStorage.setItem('goods',goods);
-            loadAll();
-        }
+        })
+    })
+
+
+    ShowCart();//先调用一次,页面加载完直接显示
+    function ShowCart(){
+        $('.dropMenu-content').remove();
+        let arr = JSON.parse(window.localStorage.getItem('cart'));
+
+        $.each(arr, function (index, value) {//each循环遍历数组,将数据一一对应添加到#DisplayInformation中并显示到页面中
+            //console.log(value);
+            let  result=
+                $(
+                    '<ul class="dropMenu-content">'+
+                    '<li>'+
+                    '<button type="button" class="close closeBtn" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+                    '<div class="clearfix"></div>'+
+                    '<article class="clearfix miniCartInfo"  id="shoppingList">'+
+                    '<div class="miniCart-left"><img src="'+value.img+'"></div>'+
+                    '<div class="miniCart-right">' +
+                    '<h4 class="card-title g-font-family-Regular g-line-height-21"><a href="">'+value.goods+'</a></h4>'+
+                    '<div class="priceAndNumber g-mt-15">' +
+                    '<h3 class="g-font-size-18 g-font-family-bold pull-left">'+value.price+'</h3>'+
+                    '<div class="input-group-add pull-right text-center">'+
+                    '<a class="jianshao" href="javascript:void(0)" href="">-</a>'+
+                    '<span type="text" class="text-center g-color-gray-dark-v4 two g-font-size-12 number" placeholder="1" aria-describedby="sizing-addon2">1 </span>'+
+                    '<a class="add1" href="javascript:void(0)">+</a>'+
+                    '</div>'+
+                    '</div>'+
+                    '</div>'+
+                    '</article>'+
+                    '</li>'+
+                    '</ul>'
+                )
+            $('#shoppingList').append(result);
+
+            let numberShow=$(
+                '<span class="number" id="number">'+value.count+'</span>'
+            )
+
+            $("#cartDropdown").append(numberShow);
+
+
+            //let numberShow=$("#number").html(value.count);
+
+        })
+
+
+
+
+
+
+
+
+
     }
 
-    function loadAll() {
-        //var list = document.getElementById("shoppingList");
-        var list = $("#shoppingList");
-        if (localStorage.length > 0) {
-            for (var i = 0; i < localStorage.length; i++) {
-                var price = localStorage.getItem("price");
-                var goods = localStorage.getItem("goods");
-                var productImg = localStorage.getItem('img');
-                //console.log(productImg)
-                result=
-                    $(
-                        '<li>'+
-                        '<button type="button" class="close closeBtn" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
-                        '<div class="clearfix"></div>'+
-                        '<article class="clearfix miniCartInfo"  id="shoppingList">'+
-                        '<div class="miniCart-left"><img src="'+productImg+'" alt="" /></div>'+
-                        '<div class="miniCart-right">' +
-                        '<h4 class="card-title g-font-family-Regular g-line-height-21"><a href="">'+goods+'</a></h4>'+
-                        '<div class="priceAndNumber g-mt-15">' +
-                        '<h3 class="g-font-size-18 g-font-family-bold pull-left">'+price+'</h3>'+
-                        '<div class="input-group-add pull-right text-center">'+
-                        '<a class="jianshao" href="javascript:void(0)" href="">-</a>'+
-                        '<span type="text" class="text-center g-color-gray-dark-v4 two g-font-size-12 number" placeholder="1" aria-describedby="sizing-addon2">1</span>'+
-                        '<a class="add1" href="javascript:void(0)">+</a>'+
-                        '</div>'+
-                        '</div>'+
-                        '</div>'+
-                        '</article>'+
-                        '</li>'
-                    )
-
-            }
-            //console.log(typeof(result));
-            list.append(result);
-        } else {
-            //list.append("购物车空空如也哦");
-            //list.innerHTML = "购物车空空如也哦……";
-        }
-
-    }
 
 
-    //点击删除
-    //$(".closeBtn").click(function() {
-    //    alert("你確定要將此產品從購物車中刪除嗎?")
-    //    $(this).parent().remove();
-    //    //prodC();
-    //});
-    //
-
-
-
-    // for循环删除购物车商品
-    let del = document.querySelectorAll('.closeBtn');
-    for (let i = 0; i < del.length; i++) {
-        del[i].onclick = function () {
-            console.log(123)
-            var val = del[i].parentNode;
-            console.log(val);
-            var children1 = val.innerText;
-            console.log(children1);
-            //var children2 = val.children[1].innerText;
-            localStorage.removeItem(children1);
-             localStorage.clear();
-            loadAll();
-        }
-    }
 
 
 
